8112951723643314eed89a92e4621dba2760d0a3
atultiwari19 <atultiwari7673@gmail.com>
2026-04-24 02:55:08 +0530

    Added Streamlit dashboard, image and MySQL integration

---
+++ dbconnect.py
@@ -0,0 +1,16 @@
+# pip install mysql
+# pip install mysql-connector-python
+import mysql
+import mysql.connector as sql
+
+db = sql.connect(
+    host = "localhost",
+    user = "root",
+    password = "Atul@1234",
+    use_pure = True
+)
+
+cr = db.cursor()
+cr.execute("show databases")
+for i in cr:
+    print(i)
