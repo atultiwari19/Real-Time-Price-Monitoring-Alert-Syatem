009b55b859255ce921599ff66524799e2b9f4fa5
atultiwari19 <atultiwari7673@gmail.com>
2026-04-24 01:44:50 +0530

    Updated Interface UI

--- Interface.py
+++ Interface.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import streamlit as st
+
+st.title("Real-Time Price Monitoring Alert System")
+
+st.header("Daily Price Tracking")
+st.header("Historical Storage")
+st.header("Price Comparison")
+st.header("Alert System for Price Changes")
+st.header("Automated Pipeline")
+
+st.markdown("Database Connective")
+
+st.success("Proceed Successfully")
+
+st.info("Find the formal information")
